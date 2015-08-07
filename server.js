b70a93b2f48b046f9da4fde91f641a6bb2d48059
roger-hamilton <roger.max.hamilton@gmail.com>
2015-08-07 09:22:28 -0500

    Setup initial server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'jade');
+
+app.use(express.static('node_modules/jquery/dist'));
+app.use(express.static('node_modules/bootstrap/dist'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening on port:%s',  port);
+});
